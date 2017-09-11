55c00981cb848a9adbdca981ac379fe17ea828d6
Cartmanishere <apbetahouse45@gmail.com>
2017-09-11 16:20:53 +0530

    9/11/2017

--- Algorithms/Implementation/sequence_equation.py
+++ Algorithms/Implementation/sequence_equation.py
@@ -1,6 +1,5 @@
 # Enter your code here. Read input from STDIN. Print output to STDOUT
 input('')
 p = [int(i) for i in input().strip().split(' ')]
-
 for i in range(1, len(p)+1):
     print(p.index(p.index(i)+1)+1)
